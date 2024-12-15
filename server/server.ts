8436154336d63b033ce034c19899129753d16f9d
MotorTruck1221 <motortruck1221@protonmail.com>
2024-12-15 03:53:41 -0700

    Bye bye masqr

--- server/server.ts
+++ server/server.ts
@@ -3,7 +3,6 @@ import fastifyCookie from '@fastify/cookie';
 import fastifyHttpProxy from '@fastify/http-proxy';
 import fastifyMiddie from '@fastify/middie';
 import fastifyStatic from '@fastify/static';
-import { masqr } from '@rubynetwork/corlink-fastify';
 import Fastify from 'fastify';
 import { serverFactory } from './serverFactory.ts';
 import { listeningMessage } from "./message.ts";
@@ -11,19 +10,9 @@ import { listeningMessage } from "./message.ts";
 const app = Fastify({ logger: false, serverFactory: serverFactory });
 
 await app.register(fastifyCookie, {
-    secret: Deno.env.get('COOKIE_SECRET') || 'e',
+    secret: Deno.env.get('COOKIE_SECRET') || 'yes',
     parseOptions: {}
 });
-if (Deno.env.get('MASQR') === 'true') {
-    await app.register(masqr, {
-        deniedFilePath: `${Deno.cwd()}/server/deniedFiles/denied.html`,
-        unlockedPaths: ['/wisp/'],
-        whiteListedURLs: [],
-        masqrUrl: 'https://corlink.example.com/validate?license=',
-        builtinCookieParser: false,
-        v3: false
-    });
-}
 await app.register(fastifyCompress, {
     encodings: ['br', 'gzip', 'deflate']
 });
@@ -36,12 +25,8 @@ await app.register(fastifyHttpProxy, {
     prefix: '/gms/',
     http2: false
 });
-let port: number;
-//if (isDocker()) {
-    port = 8080;
-//} else {
-    port = parseInt(Deno.env.get('PORT') as string) || 8000;
-//}
+
+const port = parseInt(Deno.env.get('PORT') as string) || 8000;
 
 app.listen({ port: port, host: '0.0.0.0' }).then(() => {
     listeningMessage(port, "fastify");

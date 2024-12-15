7daef27eeea3e48e1b16c844cd55bd685bf50481
MotorTruck1221 <motortruck1221@protonmail.com>
2024-12-15 03:50:39 -0700

    I need help, Incog doesn't need effing Masqr

--- server/standalone/standalone.ts
+++ server/standalone/standalone.ts
@@ -2,34 +2,11 @@ import { Hono } from "jsr:@hono/hono";
 import { serveStatic } from "jsr:@hono/hono/deno";
 import { compress } from "jsr:@hono/hono/compress";
 import { listeningMessage } from "../message.ts";
-import { Masqr, masqrAuth, type MasqrOptions } from "./masqr.ts";
 
 const app = new Hono();
 
-const options: MasqrOptions = {
-    deniedFilePath: `${Deno.cwd()}/server/deniedFiles/denied.html`,
-    v3: false,
-    unlockedPaths: [],
-    whitelistedURLS: ['incog.works'],
-    masqrURL: "https://license.mercurywork.shop/validate?license=",
-    cookieSecret: 'thisisnotsecurpleasesetsomething1234'
-}
-
-const masqr = new Masqr(options);
-
 app.use(compress({
     encoding: 'gzip'
-}));
-
-app.use(masqrAuth({
-    //Because I need hono's context to fetch the denied files (if using v3 especially), I have to define this to allow access to that context
-    getFile: (ctx) => {
-        return masqr.fail(ctx)
-    },
-    //Same with these.
-    validate: async (ctx, key, host) => {
-        return await masqr.verifyUser(ctx, key, host);
-    }
 }));
 
 app.use("/*", serveStatic({ root: `${Deno.cwd()}/dist` }));
